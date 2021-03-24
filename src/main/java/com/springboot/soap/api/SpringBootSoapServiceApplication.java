cdcc5feba7b54c32395330e46b4f23cc8654cc04
ibmpnc <80363913+ibmpnc@users.noreply.github.com>
2021-03-24 13:51:40 -0300

    Update SpringBootSoapServiceApplication.java

--- src/main/java/com/springboot/soap/api/SpringBootSoapServiceApplication.java
+++ src/main/java/com/springboot/soap/api/SpringBootSoapServiceApplication.java
@@ -2,12 +2,21 @@ package com.springboot.soap.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(value ="/")
 public class SpringBootSoapServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSoapServiceApplication.class, args);
+	}
+	
+	@RequestMapping(value ="/hai")
+	public String sayHello() {
+		return "Welcome to Spring boot application";
 	}
 
 }
